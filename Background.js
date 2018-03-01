fa09af09a8476bac9ae4b1f276640c7e9cc02982
Jung Woo Moon <jungwoomoon96@gmail.com>
2018-03-01 02:32:13 -0800

    Can upload background image. Thumbnails for default images

--- Background.js
+++ Background.js
@@ -1,91 +1,139 @@
-'use strict';
+"use strict";
 
 function Background() {
+  this.backgroundClicked = false;
 
-	this.backgroundClicked = false;
+  this.container = document.getElementById("container");
+  this.backgroundOptions = document.getElementById("backgroundOptions");
+  this.backgroundOptionsList = document.getElementById("backgroundOptionsList");
+  this.backgroundExit = document.getElementById("backgroundExit");
+  this.whiteOverlay = document.getElementById("whiteOverlay");
 
-	this.container = document.getElementById('container');
-	this.backgroundOptions = document.getElementById('backgroundOptions');
-	this.backgroundOptionsList = document.getElementById('backgroundOptionsList');
-	this.backgroundExit = document.getElementById('backgroundExit');
-	this.whiteOverlay = document.getElementById('whiteOverlay');
+  this.initialize = this.initialize.bind(this);
+  this.click = this.click.bind(this);
+  this.backgroundExit.addEventListener("click", this.click);
+  this.loadImage = this.loadImage.bind(this);
+  this.uploadFile = this.uploadFile.bind(this);
 
-	this.initialize = this.initialize.bind(this);
-	this.click = this.click.bind(this);
-	this.backgroundExit.addEventListener('click', this.click);
-	this.loadImage = this.loadImage.bind(this);
-
-	this.initialize();
+  this.initialize();
 }
 
 Background.prototype.initialize = function() {
-	var backgroundOptionsListHtml = '';
-	var firstImg = false;
-	for (var key in bgOptions) {
-		if (bgOptions[key].type == "image" && firstImg == false) {
-			firstImg = true;
-			backgroundOptionsListHtml += '<br/>';
-		}
-		backgroundOptionsListHtml +=
-			'<span class="bgOption">' +
-				'<button class="input buttonInput" id="' + key + '">' +
-					'<div class="bgOptionThumb" id="' + key + 'Thumb' + '"> </div>' +
-					key + 
-				'</button>' +
-			'</span>';
-	}
-	this.backgroundOptionsList.innerHTML = backgroundOptionsListHtml;
-	for (var key in bgOptions) {
-		(function(key, dict, container) {
-			document.getElementById(key).addEventListener('click', function() {
-				localStorage.setItem('bgKey', key);
-				var entry = dict[key];
-				if (entry.type == 'color') {
-					container.style.background = 'linear-gradient(45deg, ' + entry.value + ')';
-				} else if (entry.type == 'image') {
-					container.style.backgroundImage = 'url(' + entry.value + ')';
-					container.style.backgroundSize = 'cover';
-				}
-			});
-		}(key, bgOptions, this.container));
-	}
-	for (var key in bgOptions) {
-		var entry = bgOptions[key];
-		if (entry.type === 'color') {
-			document.getElementById(key + 'Thumb').style.backgroundColor = entry.thumb;
-		}
-	}
-}
+  var backgroundOptionsListHtml = "";
+  var firstImg = false;
+  for (var key in bgOptions) {
+    if (bgOptions[key].type == "image" && firstImg == false) {
+      firstImg = true;
+      backgroundOptionsListHtml += "<br/>";
+    }
+    backgroundOptionsListHtml +=
+      '<span class="bgOption">' +
+      '<button class="input buttonInput" id="' +
+      key +
+      '">' +
+      '<div class="bgOptionThumb" id="' +
+      key +
+      "Thumb" +
+      '"> </div>' +
+      key +
+      "</button>" +
+      "</span>";
+  }
+  this.backgroundOptionsList.innerHTML = backgroundOptionsListHtml;
+  for (var key in bgOptions) {
+    (function(key, dict, container) {
+      document.getElementById(key).addEventListener("click", function() {
+        localStorage.setItem("bgKey", key);
+        var entry = dict[key];
+        if (entry.type == "color") {
+          container.style.background =
+            "linear-gradient(45deg, " + entry.value + ")";
+        } else if (entry.type == "image") {
+          container.style.backgroundImage = "url(" + entry.value + ")";
+          container.style.backgroundSize = "cover";
+        }
+      });
+    })(key, bgOptions, this.container);
+  }
+  for (var key in bgOptions) {
+    var entry = bgOptions[key];
+    if (entry.type === "color") {
+      document.getElementById(key + "Thumb").style.backgroundColor =
+        entry.thumb;
+    }
+    if (entry.type === "image") {
+      document.getElementById(key + "Thumb").style.backgroundImage =
+        "url(" + entry.thumb + ")";
+    }
+  }
+  this.backgroundOptionsList.appendChild(document.createElement("BR"));
+  var fileChild = document.createElement("INPUT");
+  fileChild.id = "myFile";
+  fileChild.type = "file";
+  fileChild.onchange = this.uploadFile;
+  this.backgroundOptionsList.appendChild(
+    document.createElement("SPAN").appendChild(fileChild)
+  );
+};
+
+// From W3schools
+Background.prototype.uploadFile = function(event) {
+  var fileImage = event.target.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function(file) {
+    // TODO: This was supposed to trigger reading the file, saving it into localStorage, then calling loadImage
+    var entry = { type: "image", value: file.target.result };
+    localStorage.setItem("bgKey", "uploaded");
+    localStorage.setItem("uploadedFile", JSON.stringify(entry));
+    this.loadImage(entry);
+  }.bind(this);
+  reader.readAsDataURL(fileImage);
+};
 
 Background.prototype.click = function() {
-	if (this.backgroundClicked === true) {
-		this.backgroundClicked = false;
-		Velocity(this.backgroundOptions, { opacity: 0 }, { duration: transitionShort });
-		setTimeout(function() { this.backgroundOptions.style.display = 'none'; }.bind(this), transitionShort);
-	} else {
-		this.backgroundClicked = true;
-		this.backgroundOptions.style.display = 'block';
-		Velocity(this.backgroundOptions, { opacity: 1 }, { duration: transitionShort });
-	}
-}
+  if (this.backgroundClicked === true) {
+    this.backgroundClicked = false;
+    Velocity(
+      this.backgroundOptions,
+      { opacity: 0 },
+      { duration: transitionShort }
+    );
+    setTimeout(
+      function() {
+        this.backgroundOptions.style.display = "none";
+      }.bind(this),
+      transitionShort
+    );
+  } else {
+    this.backgroundClicked = true;
+    this.backgroundOptions.style.display = "block";
+    Velocity(
+      this.backgroundOptions,
+      { opacity: 1 },
+      { duration: transitionShort }
+    );
+  }
+};
 
 Background.prototype.loadImage = function(entry) {
-	if (entry.type == 'image') {
-		var img = new Image();
-		img.onload = function() {
-			this.container.style.backgroundImage = 'url(' + entry.value + ')';
-			this.container.style.backgroundSize = 'cover';
-			Velocity(this.whiteOverlay, { opacity: 0 }, { duration: transitionLong });
-			setTimeout(function() {
-				this.whiteOverlay.style.display = 'none';
-			}, transitionLong);
-		}.bind(this);
-		img.src = entry.value;
-	} else if (entry.type = 'color') {
-		this.container.style.background = 'linear-gradient(45deg, ' + entry.value + ')';
-		Velocity(this.whiteOverlay, { opacity: 0 }, { duration: transitionLong });
-		setTimeout(function() {
-			this.whiteOverlay.style.display = 'none';
-		}, transitionLong);
-	}
-}
+  if (entry.type === "image") {
+    var img = new Image();
+    img.onload = function() {
+      this.container.style.backgroundImage = "url(" + entry.value + ")";
+      this.container.style.backgroundSize = "cover";
+      Velocity(this.whiteOverlay, { opacity: 0 }, { duration: transitionLong });
+      setTimeout(function() {
+        this.whiteOverlay.style.display = "none";
+      }, transitionLong);
+    }.bind(this);
+    img.src = entry.value;
+  } else if (entry.type === "color") {
+    this.container.style.background =
+      "linear-gradient(45deg, " + entry.value + ")";
+    Velocity(this.whiteOverlay, { opacity: 0 }, { duration: transitionLong });
+    setTimeout(function() {
+      this.whiteOverlay.style.display = "none";
+    }, transitionLong);
+  }
+};
--- Until.js
+++ Until.js
@@ -1,105 +1,124 @@
-'use strict';
+"use strict";
 
 window.onload = function() {
-	window.Until = new Until();
+  window.Until = new Until();
 };
 
 var transitionShort = 300;
 var transitionLong = 900;
 var imagesLength = 3;
 var bgOptions = {
-	'color1': {'type': 'color', 'value': '#35ead5, #3493e5', 'thumb': '#3493e5'},
-	'color2': {'type': 'color', 'value': '#f44292, #f45641', 'thumb': '#f44292'},
-	'color3': {'type': 'color', 'value': '#f47641, #ebf441', 'thumb': '#f47641'},
+  color1: { type: "color", value: "#35ead5, #3493e5", thumb: "#3493e5" },
+  color2: { type: "color", value: "#f44292, #f45641", thumb: "#f44292" },
+  color3: { type: "color", value: "#f47641, #ebf441", thumb: "#f47641" }
 };
 for (var i = 0; i < imagesLength; i++) {
-	bgOptions['image' + i] = {'type': 'image', 'value': 'images/image' + i + '.jpeg'};
+  bgOptions["image" + i] = {
+    type: "image",
+    value: "images/image" + i + ".jpeg",
+    thumb: "images/thumb" + i + ".jpg"
+  };
 }
 
 function Until() {
-	this.NewCountdown = new NewCountdown();
-	this.Settings = new Settings();
-	this.Background = new Background();
+  this.NewCountdown = new NewCountdown();
+  this.Settings = new Settings();
+  this.Background = new Background();
 
-	this.countdown;
+  this.countdown;
 
-	this.bgKey = localStorage.getItem('bgKey');
+  this.bgKey = localStorage.getItem("bgKey");
 
-	this.container = document.getElementById('container');
-	this.main = document.getElementById('main');
-	this.mainDate = document.getElementById('mainDate');
-	this.mainName = document.getElementById('mainName');
+  this.container = document.getElementById("container");
+  this.main = document.getElementById("main");
+  this.mainDate = document.getElementById("mainDate");
+  this.mainName = document.getElementById("mainName");
 
-	this.newCountdown = this.newCountdown.bind(this);
-	this.clearCountdown = this.clearCountdown.bind(this);
+  this.newCountdown = this.newCountdown.bind(this);
+  this.clearCountdown = this.clearCountdown.bind(this);
 
-	// Retrieve and set background
-	if (this.bgKey == null) {
-		this.bgKey = 'color1';
-		localStorage.setItem('bgKey', 'color1');
-	}
-	var entry = bgOptions[this.bgKey];
-	this.Background.loadImage(entry);
+  var entry;
 
-	if (localStorage.getItem('initialized') !== 'true') {
-		this.newCountdown();
-	} else {
-		this.initialize();
-	}
+  // Retrieve and set background
+  if (this.bgKey == null) {
+    this.bgKey = "color1";
+    localStorage.setItem("bgKey", "color1");
+  }
+
+  if (this.bgKey == "uploaded") {
+    entry = JSON.parse(localStorage.getItem("uploadedFile"));
+  } else {
+    entry = bgOptions[this.bgKey];
+  }
+  this.Background.loadImage(entry);
+
+  if (localStorage.getItem("initialized") !== "true") {
+    this.newCountdown();
+  } else {
+    this.initialize();
+  }
 }
 
 Until.prototype.initialize = function() {
-	var name = localStorage.getItem('name');
-	var date = localStorage.getItem('date');
+  var name = localStorage.getItem("name");
+  var date = localStorage.getItem("date");
 
-	this.mainName.textContent = name;
+  this.mainName.textContent = name;
 
-	// Initialize the first second immediately
-	this.mainDate.textContent = this.counter(date);
+  // Initialize the first second immediately
+  this.mainDate.textContent = this.counter(date);
 
-	// Then every 1 second
-	this.countdown = setInterval(function() {
-		this.mainDate.textContent = this.counter(date);
-	}.bind(this), 1000);
+  // Then every 1 second
+  this.countdown = setInterval(
+    function() {
+      this.mainDate.textContent = this.counter(date);
+    }.bind(this),
+    1000
+  );
 
-	this.main.style.display = 'block';
-	Velocity(this.main, { opacity: 1, translateY: '-10px' }, { duration: transitionLong });
-	this.Settings.initialize();
-}
+  this.main.style.display = "block";
+  Velocity(
+    this.main,
+    { opacity: 1, translateY: "-10px" },
+    { duration: transitionLong }
+  );
+  this.Settings.initialize();
+};
 
 Until.prototype.newCountdown = function() {
-	this.NewCountdown.initialize();
-}
+  this.NewCountdown.initialize();
+};
 
 Until.prototype.clearCountdown = function() {
-	clearInterval(this.countdown);
-}
+  clearInterval(this.countdown);
+};
 
 Until.prototype.counter = function(date) {
-	// Locale conversion
-	var localeConvertedDate = new Date(date).getTime() + new Date().getTimezoneOffset() * 60 * 1000;
-	var diff = localeConvertedDate - Date.now();
-	if (diff <= 0) {
-		return "The time has come!"
-	}
-	var time = diff / 1000;
-	var seconds = Math.floor(time % 60);
-	time /= 60;
-	var minutes = Math.floor(time % 60);
-	time /= 60;
-	var hours = Math.floor(time % 24);
-	time /= 24;
-	var days = Math.floor(time);
-	var message = "";
-	if (days > 0) {
-		message += days + " days ";
-	}
-	if (hours > 0) {
-		message += hours + " hours ";
-	}
-	if (minutes > 0) {
-		message += minutes + " min ";
-	}
-	message += seconds + " sec";
-	return message;
-}
+  // Locale conversion
+  var localeConvertedDate =
+    new Date(date).getTime() + new Date().getTimezoneOffset() * 60 * 1000;
+  var diff = localeConvertedDate - Date.now();
+  if (diff <= 0) {
+    return "The time has come!";
+  }
+  var time = diff / 1000;
+  var seconds = Math.floor(time % 60);
+  time /= 60;
+  var minutes = Math.floor(time % 60);
+  time /= 60;
+  var hours = Math.floor(time % 24);
+  time /= 24;
+  var days = Math.floor(time);
+  var message = "";
+  if (days > 0) {
+    message += days + " days ";
+  }
+  if (hours > 0) {
+    message += hours + " hours ";
+  }
+  if (minutes > 0) {
+    message += minutes + " min ";
+  }
+  message += seconds + " sec";
+  return message;
+};
